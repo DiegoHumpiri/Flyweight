cd4fc91c0d337473c3e6a47dfedfbb9c920f96cb
Diego Johann Humpiri Vargas <dhumpiriv@ulasalle.edu.pe>
2023-05-30 06:16:08 -0500

    Ejemplo casi terminado.

--- src/flyweightBasico/BattleField.java
+++ src/flyweightBasico/BattleField.java
@@ -2,10 +2,11 @@ package flyweightBasico;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BattleField extends JFrame {
-	private ArrayList<Bullet> bullets = new ArrayList<>() ;
+	private List<Bullet> bullets = new ArrayList<>() ;
 	public void spawnBullet( int x, int y, String name, Color color, int size) {
 		BulletType type = BulletFactory.getBulletType(name, color, size );
 		Bullet bullet = new Bullet( x, y, type );
